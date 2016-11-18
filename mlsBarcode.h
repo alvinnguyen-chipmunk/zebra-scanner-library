b31ba15aeb45b6daaaf1c7ff6452b780d742ecb3
Hoang Trung Huy <huyht1205@gmail.com>
2016-11-18 14:00:38 +0700

    correct STYL licences
    Signed-off-by: Hoang Trung Huy <huyht1205@gmail.com>

--- mlsBarcode.h
+++ mlsBarcode.h
@@ -1,3 +1,15 @@
+/*******************************************************************************
+     (C) Copyright 2009 Styl Solutions Co., Ltd. , All rights reserved *
+     *
+     This source code and any compilation or derivative thereof is the sole *
+     property of Styl Solutions Co., Ltd. and is provided pursuant to a *
+     Software License Agreement. This code is the proprietary information *
+     of Styl Solutions Co., Ltd and is confidential in nature. Its use and *
+     dissemination by any party other than Styl Solutions Co., Ltd is *
+     strictly limited by the confidential information provisions of the *
+     Agreement referenced above. *
+     ******************************************************************************/
+
 #ifndef MLSQRREADER_H
 #define MLSQRREADER_H
 #ifdef __cplusplus
