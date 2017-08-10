6c9281d2c50c2aa5844d4e8af74931d8093e0133
Alvin Nguyen <alvin.nguyen@styl.solutions>
2017-08-10 08:54:51 +0700

    mlsBarcode.h: Add debug macro
    Signed-off-by: Alvin Nguyen <alvin.nguyen@styl.solutions>

--- mlsBarcode.h
+++ mlsBarcode.h
@@ -25,6 +25,19 @@ extern "C"
 #include <termios.h>
 #include <locale.h>
 
+
+#ifdef __RELEASE__
+    #define __DEBUG__(format, ...)
+#else
+    #define __DEBUG__(format, ...) fprintf (stderr, format, ## __VA_ARGS__)
+#endif // __RELEASE__
+
+#define DEBUG(format, args...) __DEBUG__("%s||%s():[%d] " format "\n",__FILE__,__FUNCTION__, __LINE__, ##args)
+#define DEBUG_1(format, ...) DEBUG("\n"      format, ##__VA_ARGS__)
+#define DEBUG_0() DEBUG("\n")
+#define STYL_DEBUG(format, ...) DEBUG("\n      "format, ##__VA_ARGS__)
+
+
 /*!
  * \brief mlsBarcodeReader_Open Open Reader descritptor file for read write
  * \return
@@ -37,7 +50,7 @@ char mlsBarcodeReader_Open(char *name);
  * \brief mlsBarcodeReader_Enable Enable Reader for scaning QR code/Bar Code
  * \return
  * - EXIT_SUCCESS: Success
- * - EXIT_SUCCESS: Fail
+ * - EXIT_FAILURE: Fail
  */
 char mlsBarcodeReader_Enable();
 
@@ -45,7 +58,7 @@ char mlsBarcodeReader_Enable();
  * \brief mlsBarcodeReader_Disable Disable reader, Reader can't scan any QR code/bar code
  * \return
  * - EXIT_SUCCESS: Success
- * - EXIT_SUCCESS: Fail
+ * - EXIT_FAILURE: Fail
  */
 char mlsBarcodeReader_Disable();
 
@@ -60,14 +73,14 @@ unsigned int mlsBarcodeReader_ReadData(char *buff, const int buffLength, const i
  * \brief mlsBarcodeReader_close close Reader file descriptor
  * \return
  * - EXIT_SUCCESS: Success
- * - EXIT_SUCCESS: Fail
+ * - EXIT_FAILURE: Fail
  */
 char mlsBarcodeReader_Close();
 
 /*!
  * \brief GetVersion provide software version
  * \return string of software version
- * - 
+ * -
  */
 char *GetVersion(void);
 
@@ -75,7 +88,7 @@ char *GetVersion(void);
  * \brief mlsBarcodeReader_reopen() closes and re-opens scanner device file
  * \return
  * - EXIT_SUCCESS: Success
- * - EXIT_SUCCESS: Fail
+ * - EXIT_FAILURE: Fail
  */
 char mlsBarcodeReader_Reopen(char *name);
 
